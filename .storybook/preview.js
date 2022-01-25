10ceec8ebb827c1d23defd50a3906b25dfe10b4a
Joyce <yeechiajoyce@gmail.com>
2022-01-25 17:13:14 +0800

    build: 設定storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import theme from "../src/styles/abstracts/theme";
+
+export const decorators = [
+  (story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>,
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
